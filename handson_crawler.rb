6c8c2916c338240885e541f6c27067754ef7dff8
jczetta <jczetta@gmail.com>
2012-08-13 11:58:21 -0400

    scrape needs cleanup, have base and goal urls with a few errors to correct. new file for crawling

---
+++ handson_crawler.rb
@@ -0,0 +1,17 @@
+## more hands-on web crawler
+# possible combinations
+# some code from http://www.skorks.com/2009/07/how-to-write-a-web-crawler-in-ruby/ (maybe)
+# some code from The Bastards Book of Ruby: http://ruby.bastardsbook.com/chapters/web-crawling/
+
+require 'rubygems'
+require 'open-uri'
+require 'nokogiri'
+require 'mechanize'
+require 'scrape' # will this include scrape.rb?
+
+
+
+
+
+
+BASE_
--- scrape.rb
+++ scrape.rb
@@ -28,8 +28,8 @@ if nr =~ /^en\.wikipedia/ || nr =~ /.svg$/
   f = poss.links.sample
   nr = f.uri.to_s
 end
-
-puts "Your start page is #{f}. \n URL: http://en.wikipedia.org#{nr}"
+st_url = "http://en.wikipedia.org#{nr}"
+puts "Your start page is #{f}. \n URL: #{st_url}"
 
 # 2: get the goal page
 sec_agent = Mechanize.new { |ag| 
@@ -42,8 +42,8 @@ pt = tl.uri.to_s
 
 finstr = tl.to_s.gsub(' ', '%20')
 if finstr =~ /[Hh]elp/ || finstr =~ /[Ff]ile/ || finstr =~ /Meta/
-  ntl = pot.links.sample
-  finstr = ntl.to_s.gsub(' ', '%20')
+  tl = pot.links.sample
+  finstr = tl.to_s.gsub(' ', '%20')
 end
 url = "https://en.wikipedia.org/#{finstr}"
 
@@ -52,10 +52,40 @@ url = "https://en.wikipedia.org/#{finstr}"
 puts "Your goal page is #{tl}. \n URL: #{url}"
 #puts "Wait a moment at the page for redirection if it seems odd. <msg about contacting re: errors>"
 
+
+
 # occasional problems remain, e.g. https://en.wikipedia.org/wiki/%D0%A1%D1%80%D0%BF%D1%81%D0%BA%D0%B8_/_srpski
 # (it's not an english page so there is nothing to show, no reasonable redirection)
 # check text on page?
 
+ #####
+
+# quit crawling/searching if the time diff is greater than 120, 'cos it measures in seconds (POSIX?)
+# see if that's reasonable time for Ruby
 
 # 3: crawl until successful FTW
-#start_url = 
+start_url = st_url
+goal_url =  url
+goal_title = tl.to_s
+
+
+
+
+
+
+# nag = Mechanize.new { |ag|
+#   ag.user_agent_alias = 'Mac Safari'
+# }
+
+#nxt_pg = ''
+# bgn_pg = nag.get(start_url)
+
+# want all that have 
+## ___<a href="/wiki/___ + \w*
+# poss_links = bgn_pg.links.find_all { |ls| ls.attributes.parent.name == 'a href'  }
+# puts "here are poss links, #{poss_links}"
+
+
+# until nxt_pg == goal_title do
+#   
+# end
