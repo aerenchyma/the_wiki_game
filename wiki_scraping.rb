7a36d17496949dd7adf9adf2f5fdc00bb2cc12c5
jczetta <jczetta@gmail.com>
2012-08-14 16:50:11 -0400

    working win -- about to conciseify

--- wiki_scraping.rb
+++ wiki_scraping.rb
@@ -7,15 +7,21 @@ def get_links(page_url)
   ndoc = Nokogiri::HTML(open(page_url))
   links = ndoc.css('a')
   tmp = []
+  found = nil
   # hrefs = links.map {|ln| ln.attribute('href').to_s}
   links.each do |ln|
+    #found = nil
     if ln.text == "next 5,000"
       # puts ln.text
       #     puts ln["href"]
       tmp = get_links("http://en.wikipedia.org" + ln["href"])
       tmp << "http://en.wikipedia.org" + ln["href"]
+      found = true
       break
     end
+  end
+  if found == nil
+    tmp << page_url
   end
   tmp
 end
@@ -66,8 +72,10 @@ if finstr =~ /[Hh]elp/ || finstr =~ /[Ff]ile/ || finstr =~ /Meta/
 end
 url = "https://en.wikipedia.org/#{finstr}"
 
-start_name = f.to_s.gsub(' ', '%20')
-goal_name = tl.to_s.gsub(' ', '%20')
+#start_name = f.to_s.gsub(' ', '%20')
+start_name = "England"
+#goal_name = tl.to_s.gsub(' ', '%20')
+goal_name = "Jane%20Austen"
 
 #puts start_name
 
@@ -88,9 +96,9 @@ WikiNode = Struct.new(:link, :parent, :text)
 wiki_arr = []
 
 # initializing array (real or testing):
-
-#wiki_arr << WikiNode.new(goal_name,-1)
-wiki_arr << WikiNode.new("2007", -1)
+puts  " link: #{baselinks_url+goal_name}, text: #{goal_name}"
+wiki_arr << WikiNode.new(baselinks_url + goal_name,-1, goal_name)
+#wiki_arr << WikiNode.new("2007", -1)
 
 #wikidoc = Nokogiri::HTML(open(baselinks_url + wiki_arr[0].link))
 
@@ -98,51 +106,69 @@ wiki_arr << WikiNode.new("2007", -1)
 
 
 # full url to start with
-curr_url = baselinks_url + wiki_arr[0].link + "\&limit=5000"
-
-
-# actual array of links
-arr = get_links(curr_url)
+# curr_url = baselinks_url + wiki_arr[parent].link + "\&limit=5000"
+# 
+# 
+# # actual array of links
+# arr = get_links(curr_url)
 
 
 #xpathstr = ".//ul[@id='mw-whatlinkshere-list']/li[#{num}]/a" ## xpath str example
 
 parent = 0
-pg = new_agent.get(arr[0]) # error: arr[0] seems to begin /w/ ... missing the full url getting: where?
-(1..10).each do |ck|
-  txt = Nokogiri::HTML(pg.body).xpath(".//ul[@id='mw-whatlinkshere-list']/li[#{ck}]/a/text()").to_s.gsub(' ','%20')
-  t = Nokogiri::HTML(pg.body).xpath(".//ul[@id='mw-whatlinkshere-list']/li[#{ck}]/span[@class='mw-whatlinkshere-tools']/a/@href").to_s.gsub(' ', '%20')
-  if t == ""
-    break
-  else
-    wiki_arr << WikiNode.new("en.wikipedia.org" + t, parent, txt)
-    # p t
-    # p txt
+win = nil
+while wiki_arr.length > parent && parent < 5
+  puts "parent is now: #{parent}"
+
+  curr_url = wiki_arr[parent].link + "\&limit=5000"
+  puts curr_url
+  arr = get_links(curr_url)
+  p arr
+  pg = new_agent.get(arr[0]) # error: arr[0] seems to begin /w/ ... missing the full url getting: where?
+  # (1..10).each do |ck|
+  #   txt = Nokogiri::HTML(pg.body).xpath(".//ul[@id='mw-whatlinkshere-list']/li[#{ck}]/a/text()").to_s.gsub(' ','%20')
+  #   t = Nokogiri::HTML(pg.body).xpath(".//ul[@id='mw-whatlinkshere-list']/li[#{ck}]/span[@class='mw-whatlinkshere-tools']/a/@href").to_s.gsub(' ', '%20')
+  #   if t == ""
+  #     break
+  #   else
+  #     wiki_arr << WikiNode.new("http://en.wikipedia.org" + t, parent, txt)
+  #     if txt == start_name
+  #       puts "Win"
+  #       win = true
+  #       break
+  #     end
+  #     p t
+  #     p txt
+  #   end
+  # end
+  # 
+  # 
+  # arr.shift # pops first element off
+
+  if not arr.empty?
+    if win 
+       break
+    end
+    arr.each do |elem|
+     # wikidoc = Nokogiri::HTML(open(elem))
+      pg = new_agent.get(elem)
+      (1..10).each do |num|
+        txt = Nokogiri::HTML(pg.body).xpath(".//ul[@id='mw-whatlinkshere-list']/li[#{num}]/a/text()").to_s.gsub(' ', '%20')
+        t = Nokogiri::HTML(pg.body).xpath(".//ul[@id='mw-whatlinkshere-list']/li[#{num}]/span[@class='mw-whatlinkshere-tools']/a/@href").to_s.gsub(' ', '%20')
+        wiki_arr << WikiNode.new("http://en.wikipedia.org" + t, parent, txt)
+        if txt == start_name
+          puts "Win"
+          win = true
+          break
+        end
+       p t
+       p txt
+      end
+    end
   end
-end
-
-arr.shift # pops first element off
-
-arr.each do |elem|
- # wikidoc = Nokogiri::HTML(open(elem))
-  pg = new_agent.get(elem)
-  (1..10).each do |num|
-    txt = Nokogiri::HTML(pg.body).xpath(".//ul[@id='mw-whatlinkshere-list']/li[#{num}]/a/text()").to_s.gsub(' ', '%20')
-    t = Nokogiri::HTML(pg.body).xpath(".//ul[@id='mw-whatlinkshere-list']/li[#{num}]/span[@class='mw-whatlinkshere-tools']/a/@href").to_s.gsub(' ', '%20')
-    wiki_arr << WikiNode.new("en.wikipedia.org" + t, parent, txt)
-    # p t
-    # p txt
+  if win 
+     break
   end
-end
-
-
-
-while wiki_arr.length > parent
-  if wiki_arr[parent].text == start_name
-    puts "Win"
-  end
-  
-  
   parent += 1
 end
 
