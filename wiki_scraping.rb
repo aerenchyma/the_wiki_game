4e94882867787c50a07ddc8d3543a230a51617af
jczetta <jczetta@gmail.com>
2012-10-02 13:29:25 -0300

    update comments, cleanup

--- wiki_scraping.rb
+++ wiki_scraping.rb
@@ -2,6 +2,8 @@ require 'open-uri'
 require 'nokogiri'
 require 'mechanize'
 
+# heavily commented for learning
+
 # function to get array of depth-1 links
 def get_links(page_url)
   ndoc = Nokogiri::HTML(open(page_url))
@@ -26,22 +28,12 @@ def print_path(win_node)
     curr_node = win_node
     while curr_node.parent != nil
       puts curr_node.text
-      #curr_node = $wiki_arr[curr_node.parent]
       curr_node = $wiki_hash[curr_node.parent]
     end
     puts curr_node.text
 end
 
-# def in_previous(text)
-#   $wiki_arr.each do |c|
-#     if c.text == text
-#       return true
-#     end
-#   end
-#   return false
-# end
-
-# CODE to get start and goal links
+# begin code to get start and goal links
 
 agent = Mechanize.new { |ag|
   ag.user_agent_alias = 'Mac Safari'
@@ -55,7 +47,7 @@ f = poss.links.sample
 nr = f.uri.to_s
 
 if nr =~ / /
-  nr.gsub!(' ','%20') # TODO url encode, without the check
+  nr.gsub!(' ','%20') # non-wikipedia require more complex urlencode
 end
 if nr =~ /^\w/
   nr.insert(0, '/')
@@ -87,6 +79,8 @@ if finstr =~ /[Hh]elp/ || finstr =~ /[Ff]ile/ || finstr =~ /Meta/
 end
 url = "https://en.wikipedia.org/#{finstr}"
 
+# testing and wins:
+
 #start_name = f.to_s.gsub(' ', '%20') 
 #start_name = "1792" # engineered win
 #goal_name = tl.to_s.gsub(' ', '%20') 
@@ -94,11 +88,10 @@ url = "https://en.wikipedia.org/#{finstr}"
 start_name = "Hacker%20(term)" # engineering again
 goal_name = "Ruby%20on%20Rails" # engineering again
 
-###### END CODE to get start and goal links
+###### end codeto get start and goal links
 
-#baselinks_url = "http://en.wikipedia.org/wiki/Special:WhatLinksHere/"
+
 baselinks_url = "http://en.wikipedia.org/w/index.php?title=Special:WhatLinksHere/"
-
 
 new_agent = Mechanize.new { |ag|
   ag.user_agent_alias = 'Mac Safari'
@@ -109,27 +102,24 @@ WikiNode = Struct.new(:link, :parent, :text)
 
 # array of Nodes
 $wiki_arr = []
-
 # initializing hashes
 $wiki_hash = {}
-
-# initializing array (real or testing):
+# initializing array (real or testing) to watch process in command line:
 puts  " link: #{baselinks_url+goal_name}, text: #{goal_name}"
-#$wiki_arr << WikiNode.new(baselinks_url + goal_name,-1, goal_name)
-$wiki_hash[goal_name] = WikiNode.new(baselinks_url + goal_name, nil, goal_name)
+$wiki_hash[goal_name] = WikiNode.new(baselinks_url + goal_name, nil, goal_name) # using Ruby 1.9 hash, retains order
 
-#$wiki_arr << WikiNode.new("2007", -1)
+#$wiki_arr << WikiNode.new("2007", -1) # for testing -- 2007 longest wiki page as of writing
 
 parent = 0
 win = nil
-while $wiki_hash.keys.length > parent #&& parent < 5
+while $wiki_hash.keys.length > parent 
   puts "parent is now: #{parent}"
   sleep 0.1 # delay .1 secs
   curr_url = $wiki_hash[$wiki_hash.keys[parent]].link + "\&limit=5000"
-  #puts curr_url
+  #puts curr_url -- for testing
   parent_text = $wiki_hash[$wiki_hash.keys[parent]].text
   arr = get_links(curr_url)
-  #p arr
+  #p arr -- for testing
   pg = new_agent.get(arr[0]) 
 
 
@@ -150,7 +140,6 @@ while $wiki_hash.keys.length > parent #&& parent < 5
         else
           p txt # so you can see where it's going
           if !$wiki_hash.has_key?(txt)
-            #$wiki_arr << WikiNode.new("http://en.wikipedia.org" + t, parent, txt)
             $wiki_hash[txt] = WikiNode.new("http://en.wikipedia.org" + t, parent_text, txt)
           end
         end
